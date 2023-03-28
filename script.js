1844ba6cf4e334ee3f78a271f5312ddca48f3d09
keshavjindal <kes333hav@gmail.com>
2023-03-28 16:39:31 +0530

    minnesota added

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ function updateTime() {
     const delhiTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"});
     const pennsylvaniaTime = new Date().toLocaleString("en-US", {timeZone: "America/New_York"});
     const arizonaTime = new Date().toLocaleString("en-US", {timeZone: "America/Phoenix"});
+    const minnesotaTime = new Date().toLocaleString("en-US", {timeZone: "America/Chicago"});
   
     // Update the HTML with the current time in each location
     document.getElementById("delhiClock").innerHTML = delhiTime;
     document.getElementById("pennsylvaniaClock").innerHTML = pennsylvaniaTime;
     document.getElementById("arizonaClock").innerHTML = arizonaTime;
+    document.getElementById("minnesotaClock").innerHTML = minnesotaTime;
   }
   
   setInterval(updateTime, 1000);
